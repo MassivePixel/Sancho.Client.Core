07c666c274a90987fb66be4fb7732badc6f511ba
Toni Petrina <tpetrina@tpetrina.com>
2016-07-24 11:40:28 +0200

    Added plugin test

---
+++ samples/PluginTest/Sancho.Client.PluginTest/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace Sancho.Client.PluginTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Sancho.Client.PluginTestPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+
---
+++ samples/PluginTest/Sancho.Client.PluginTest/Sancho.Client.PluginTestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Sancho.Client.PluginTest
+{
+    public partial class PluginTestPage : ContentPage
+    {
+        public PluginTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
