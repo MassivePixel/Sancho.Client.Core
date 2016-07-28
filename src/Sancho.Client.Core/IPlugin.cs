b7a96ab49a061fe1ae2d7557d75d90e837739b82
Toni Petrina <tpetrina@tpetrina.com>
2016-07-28 13:22:46 +0200

    Added license header to all code files

--- src/Sancho.Client.Core/IPlugin.cs
+++ src/Sancho.Client.Core/IPlugin.cs
@@ -1,4 +1,6 @@
-﻿namespace Sancho.Client.Core
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+namespace Sancho.Client.Core
 {
     public interface IPlugin
     {
